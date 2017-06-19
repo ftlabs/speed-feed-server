4ed0965334d68009d2ef36da0786a7f62065d16a
Lily2point0 <lily.madar@ft.com>
2017-06-19 14:54:38 +0100

    handle CORS on the server

--- server/index.js
+++ server/index.js
@@ -17,6 +17,11 @@ if (process.env.NODE_ENV !== 'local') {
 	app.use(helmet());
 	app.enable('trust proxy');
 	app.use(express_enforces_ssl());
+	app.use(function(req, res, next) {
+		res.header("Access-Control-Allow-Origin", "*.ft.com");
+		res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+		next();
+	});
 } else {
 	app.use(express.static(path.resolve(__dirname + "/../public")));
 
